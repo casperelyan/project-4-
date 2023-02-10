cbe6cddd3fb3f915830a3ced445be8d752a739aa
Lucas <4h4photography@gmail.com>
2023-02-10 13:28:18 +0000

    Add welcome back page with buttons

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ function togglePasswordVisibility() {
   const passwordInput = document.getElementById("password");
   passwordInput.type = passwordInput.type === "password" ? "text" : "password";
 }
+
+//use user name provided in form to create welcome back message
+const form = document.getElementById("formId");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const name = form.elements["name"].value;
+    document.getElementById("welcomeMessage").innerHTML = `Welcome back, ${name}!`;
+  });
